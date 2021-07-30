6bb2420ca2bbec0c75cb91cc560e694b39a9bd86
itasworkshop <mepraveens121@gmail.com>
2021-07-31 01:25:06 +0530

    31 July

--- cpm_app/src/main/kotlin/app/TestTaskJPA.kt
+++
@@ -1,9 +0,0 @@
-package app
-
-import entity.TaskDAO
-
-fun main(args:Array<String>) {
-    TaskDAO().findAllTask().forEach {
-            task -> println("${task.name} ---\\${task.cost}---\\${task.dependencies}")
-    }
-}
